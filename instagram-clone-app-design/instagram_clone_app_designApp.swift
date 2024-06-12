d8ac27c356403d9b89fd3f55fb7157cf7b98aeb3
Sevde Aydın <sevdenuraydn7@gmail.com>
2024-06-12 23:30:08 +0300

    Initial Commit

---
+++ instagram-clone-app-design/instagram_clone_app_designApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagram_clone_app_designApp.swift
+//  instagram-clone-app-design
+//
+//  Created by Sevde Aydın on 12.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct instagram_clone_app_designApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
