33b3e34b27bed2c3e877b723a9a2bfe17eb2665e
Sevde Aydın <sevdenuraydn7@gmail.com>
2024-06-14 23:05:02 +0300

    added self profile design

---
+++ instagram-clone-app-design/Views/SelfProfile.swift
@@ -0,0 +1,18 @@
+//
+//  SelfProfile.swift
+//  instagram-clone-app-design
+//
+//  Created by Sevde Aydın on 14.06.2024.
+//
+
+import SwiftUI
+
+struct SelfProfile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelfProfile()
+}
