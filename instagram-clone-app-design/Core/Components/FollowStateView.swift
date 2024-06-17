6e5fd9eee990036404a96468f9f28229b67535dd
Sevde Aydın <sevdenuraydn7@gmail.com>
2024-06-18 01:13:03 +0300

    profile view updated

---
+++ instagram-clone-app-design/Core/Components/FollowStateView.swift
@@ -0,0 +1,18 @@
+//
+//  FollowStateView.swift
+//  instagram-clone-app-design
+//
+//  Created by Sevde Aydın on 17.06.2024.
+//
+
+import SwiftUI
+
+struct FollowStateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FollowStateView()
+}
---
+++ instagram-clone-app-design/Core/TabBar/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  instagram-clone-app-design
+//
+//  Created by Sevde Aydın on 17.06.2024.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
