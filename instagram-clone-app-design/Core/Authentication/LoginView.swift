04f4526ed8123068205e86fb378500489760ae97
Sevde Aydın <sevdenuraydn7@gmail.com>
2024-06-22 00:03:42 +0300

    updated login view

--- instagram-clone-app-design/Core/Authentication/LoginView.swift
+++ instagram-clone-app-design/Core/Authentication/LoginView.swift
@@ -8,8 +8,71 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Spacer()
+            Image("insta-logo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 220, height: 100)
+            
+            VStack(spacing: 8) {
+                TextField("Phone number, username or email", text: $email)
+                    .autocapitalization(.none)
+                    .font(.subheadline)
+                    .padding(12)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(10)
+                    .padding(.horizontal, 24)
+                
+                SecureFieldWithButton("Password", text: $password)
+            }
+            
+            LoginButton(text: "Login")
+                .padding(.top, 10)
+            
+            Button {
+                
+            } label: {
+                Text("Forget Passoword?")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                    .padding(.top)
+                    .padding(.trailing, 20)
+            }.frame(maxWidth: .infinity, alignment: .trailing)
+            
+            HStack {
+                Rectangle()
+                    .frame(height: 0.5)
+                Text("OR")
+                Rectangle()
+                    .frame(height: 0.5)
+            }
+            .foregroundColor(.gray)
+            .padding(.horizontal, 24)
+            
+            HStack {
+                Text("Continue with FaceBook")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(Color(.systemBlue))
+            }.padding(.top, 8)
+            
+            Spacer()
+            Divider()
+            NavigationLink {
+                AddEmailView()
+                    .navigationBarBackButtonHidden(true)
+            } label: {
+                HStack {
+                    Text("Don't you have an account?")
+                    Text("Sign up")
+                        .fontWeight(.semibold)
+                }
+            }.padding(.vertical)
+        }
     }
 }
 
