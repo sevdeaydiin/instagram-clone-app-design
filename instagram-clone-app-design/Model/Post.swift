fe27ef5fcd62ed1909b974adbd8527b0a4e942b3
Sevde Aydın <sevdenuraydn7@gmail.com>
2024-06-22 15:44:09 +0300

    add post model

--- instagram-clone-app-design/Model/Post.swift
+++ instagram-clone-app-design/Model/Post.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+struct Post: Identifiable, Hashable, Codable {
+    let id: String
+    let ownerUid: String
+    let caption: String
+    var likes: Int
+    let imageUrl: String
+    let timestamp: Date
+    var user: User?
+}
+
+extension Post {
+    static var MOCK_POST: [Post] = [
+        .init(id: UUID().uuidString, ownerUid: UUID().uuidString, caption: "🦋", likes: 354, imageUrl: "image2", timestamp: Date(), user: User.MOCK_USER[0]),
+        .init(id: UUID().uuidString, ownerUid: UUID().uuidString, caption: "♥️", likes: 354, imageUrl: "ahmet", timestamp: Date(), user: User.MOCK_USER[1]),
+    ]
+}
